75a622fff50ed92e68d3f22261f98d778a46a7fb
BlaxLinux <abdullahismael078@gmail.com>
2025-06-05 01:42:03 +0300

    but what is pathname

--- freepik-image-store/server/lib/downloadlogic.js
+++ freepik-image-store/server/lib/downloadlogic.js
@@ -111,6 +111,7 @@ async function downloadWorkerLogic({ userId, downloadLink, page }) {
       res => {
         console.log(res);
         const url = res.url();
+        const pathname = url.pathname.toLowerCase();
         console.log('🔍 response URL:', url);
         return (
           ( pathname.endsWith('.jpg') ||
